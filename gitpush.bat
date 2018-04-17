2cc52c2c44e10bd281cacbc746587d261b5b4e1b
charliewp <cprice9739@carolina.rr.com>
2018-04-17 15:14:29 -0400

    ΓÇ£check push scriptΓÇ¥

--- gitpush.bat
+++ gitpush.bat
@@ -4,10 +4,9 @@ set month=%date:~4,2%
 set day=%date:~7,2%
 
 echo  %year%.%month%.%day%.%1
-
+git status
+prompt
 REM all of the files, set commit and tag values
 git add .
 git commit -m “%1”
-REM git tag -a "Rel %year%.%month%.%day%"  -m "%1"
-
 git push https://github.com/charliewp/Guestbook.git master
