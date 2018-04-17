f9a9284808a8c04a918c8d8da58af1ad8b47a18c
charliewp <cprice9739@carolina.rr.com>
2018-04-17 15:07:13 -0400

    ΓÇ£testΓÇ¥

--- gitpush.cmd
+++ gitpush.cmd
@@ -1,9 +1,9 @@
 set year=%date:~10,4%
 set month=%date:~4,2%
 set day=%date:~7,2%
-
-echo  %year%.%month%.%day%.%1
 echo off
+echo  %year%.%month%.%day%.%1
+
 REM all of the files, set commit and tag values
 git add .
 git commit -m “%1”
