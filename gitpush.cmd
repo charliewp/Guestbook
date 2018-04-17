a55c379e0ad0e80a03b1964d72e1d3571a001e0c
charliewp <cprice9739@carolina.rr.com>
2018-04-17 15:09:14 -0400

    ΓÇ£testΓÇ¥

--- gitpush.cmd
+++ gitpush.cmd
@@ -8,6 +8,6 @@ echo  %year%.%month%.%day%.%1
 REM all of the files, set commit and tag values
 git add .
 git commit -m “%1”
-git tag -a "Rel %year%.%month%.%day%"  -m "%1"
+REM git tag -a "Rel %year%.%month%.%day%"  -m "%1"
 
 REM git push https://github.com/charliewp/Guestbook.git master
